5728adb507ca8a6ad01b67fb2b4767b2a0e6a128
Mykhail1387 <melnychukmykhailo87@gmail.com>
2019-11-30 16:48:01 +0200

    my sixth homework, redesigned

--- js/tasks_1-10.js
+++ js/tasks_1-10.js
@@ -4,90 +4,74 @@ import users from './users.js';
 
 //===================================task-01.js===========:
 
-const getUserNames = users => {
-    return users.map(user => user.name)
-};
+const getUserNames = users => users.map(({ name }) => name);
 
 console.log(getUserNames(users));
 
 //===================================task-02.js===========:
 
-
-const getUsersWithEyeColor = (users, color) => {
-    return users.filter(user => user.eyeColor === color)
-};
+const getUsersWithEyeColor = (users, color) => users.filter(user => user.eyeColor === color);
 
 console.log(getUsersWithEyeColor(users, 'blue'));
 
-//===================================task-03.js===========:
+// ===================================task-03.js===========:
 
-const getUsersWithGender = (users, gender) => {
-    const namesUsersByGender = users.filter(user => user.gender === gender);
-    return namesUsersByGender.map(user => user.name);
-};
+const getUsersWithGender = (users, genderSex) => users
+    .filter(({ gender }) => gender === genderSex)
+    .map(({ name }) => name);
 
 console.log(getUsersWithGender(users, 'male'));
 
-//===================================task-04.js===========:
+// ===================================task-04.js===========:
 
-const getInactiveUsers = users => {
-    return users.filter(user => !user.isActive)
-};
+const getInactiveUsers = users => users.filter(({ isActive }) => !isActive);
 
 console.log(getInactiveUsers(users));
 
-//===================================task-05.js===========:
+// ===================================task-05.js===========:
 
-const getUserWithEmail = (users, email) => {
-    return users.filter(user => user.email === email)
-};
+const getUserWithEmail = (users, email) => users.find(user => user.email === email);
 
 console.log(getUserWithEmail(users, 'shereeanthony@kog.com'));
 console.log(getUserWithEmail(users, 'elmahead@omatom.com'));
 
-//===================================task-06.js===========:
+// ===================================task-06.js===========:
 
-const getUsersWithAge = (users, min, max) => {
-    return users.filter(user => user.age >= min && user.age <= max)
-};
+const getUsersWithAge = (users, min, max) => users.filter(({ age }) => age >= min && age <= max);
+
 console.log(getUsersWithAge(users, 20, 30));
 console.log(getUsersWithAge(users, 30, 40));
 
-//===================================task-07.js===========:
+// ===================================task-07.js===========:
 
-const calculateTotalBalance = users => {
-    const initialValue = 0;
-    return users.reduce((accumulator, currentValue) => accumulator + currentValue.balance, initialValue)
-};
+const calculateTotalBalance = users => users.reduce((accumulator, { balance }) => accumulator + balance, 0);
 
 console.log(calculateTotalBalance(users));
 
-//===================================task-08.js===========:
+// ===================================task-08.js===========:
 
-const getUsersWithFriend = (users, friendName) => {
-    const usersFriends = users.filter(user => user.friends.includes(friendName));
-    return usersFriends.map(user => user.name);
-};
+const getUsersWithFriend = (users, friendName) => users
+    .filter(({ friends }) => friends.includes(friendName))
+    .map(({ name }) => name);
 
 console.log(getUsersWithFriend(users, 'Briana Decker'));
 console.log(getUsersWithFriend(users, 'Goldie Gentry'));
 
-//===================================task-09.js===========:
+// ===================================task-09.js===========:
 
-const getNamesSortedByFriendsCount = users => {
-    const sortByNumberFriends = users.sort((a, b) => a.friends.length - b.friends.length);
-    return sortByNumberFriends.map(user => user.name)
-};
+const getNamesSortedByFriendsCount = users => users
+    .sort((a, b) => a.friends.length - b.friends.length)
+    .map(({ name }) => name);
 
 console.log(getNamesSortedByFriendsCount(users));
 
-//===================================task-10.js===========:
+// ===================================task-10.js===========:
 
 const getSortedUniqueSkills = users => {
     const resolt = users
         .reduce((acc, skil) => {
-            acc.push(...skil.skills)
-            return acc;
+            const allSkills = acc.concat(...skil.skills)
+            return allSkills;
         }, [])
 
         .filter((item, index, skills) => skills.indexOf(item) === index)
